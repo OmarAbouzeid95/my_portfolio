dc879ecfa9387dd9515de1f198513d5893a3bafb
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2024-08-01 23:04:11 -0400

    added custom screens and fixed intro fonts

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,16 +3,16 @@ import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center font-extrabold p-8">
+    <div className="h-screen flex flex-col justify-center items-center font-extrabold p-8 lsm:p-14">
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.5 }}
         viewport={{ once: true }}>
-        <h1 className="text-5xl mb-4 w-fit md:text-6xl md:mb-6 xl:text-7xl">
+        <h1 className="text-4xl msm:text-5xl mb-4 w-fit md:text-6xl md:mb-6 xl:text-7xl">
           Hi, I'm Omar
         </h1>
-        <h2 className="text-4xl w-fit md:text-5xl md:mb-6 xl:text-6xl">
+        <h2 className="text-3xl msm:text-4xl w-fit md:text-5xl md:mb-6 xl:text-6xl">
           I'm a Full Stack Developer
         </h2>
       </motion.div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,15 @@ const config = {
   ],
   prefix: "",
   theme: {
+    screens: {
+      msm: "375px",
+      lsm: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       backgroundColor: {
         dark: "#292828",
