d6ed0983f430e9ced68b5269aa2910becd41d8a7
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2024-08-02 22:57:03 -0400

    update: added metadata and changed favicon

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -3,6 +3,12 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Resume from "@/components/Resume";
 import Contact from "@/components/Contact";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Omar's portfolio",
+  description: "Personal portfolio of Omar showcasing skills and experiences.",
+};
 
 const Home = () => {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import Projects from "@/components/Projects";
 import Resume from "@/components/Resume";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Omar's portfolio",
+  description: "Personal portfolio of Omar showcasing skills and experiences.",
+};
 
 const Home = () => {
   return (
